7c0140b337267aa344f6460b032de955179e99ec
Fitriaevis <fitriaevi251103@gmail.com>
2023-10-17 13:14:11 +0700

    modul_14_sampai_18

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express') // membuat variable baru dengan nama express
 const app = express() // membuat variable baru dengan nama app yang isinya express
 const port = 3000 // membuat variable dengan nama port yang isinya 3000
+const cors = require('cors')
+app.use(cors())
+
 
 // app.get('/', (req,res)=>{
 //     res.send('Halo Saya Fitria Evi Susana')
